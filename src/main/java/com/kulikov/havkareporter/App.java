57eab8538b02baaa020eb8ab94e2433700acaee4
gene <qa.in.gene@gmail.com>
2015-12-28 13:28:02 +0200

    App was updated

--- src/main/java/com/kulikov/havkareporter/App.java
+++ src/main/java/com/kulikov/havkareporter/App.java
@@ -2,21 +2,28 @@ package com.kulikov.havkareporter;
 
 import com.skype.*;
 
+import javax.swing.*;
+import java.io.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 /**
  * Created by Gene on 23-12-2015.
- *
  */
 public class App {
-
 
     public static void main(String[] args) throws Exception {
 
         ConsoleHavkaReporter consoleHavkaReporter = ConsoleHavkaReporter.getInstance();
-        consoleHavkaReporter.printReport();
+        try{
+            consoleHavkaReporter.printReport();
+        }
+        catch (com.skype.NotAttachedException e){
+            consoleHavkaReporter.closeProgram();
+            JOptionPane.showMessageDialog(null, "Login to your skype and re-launch program");
+        }
     }
 }
 
