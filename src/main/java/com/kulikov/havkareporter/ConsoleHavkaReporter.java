b1e2e7d1a37ba8acb673cfe65b28cbb50eb07be1
gene <qa.in.gene@gmail.com>
2015-12-28 13:26:06 +0200

    ConsoleHavkaReporter was updated

--- src/main/java/com/kulikov/havkareporter/ConsoleHavkaReporter.java
+++ src/main/java/com/kulikov/havkareporter/ConsoleHavkaReporter.java
@@ -2,7 +2,6 @@ package com.kulikov.havkareporter;
 
 import com.skype.*;
 
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -29,22 +28,73 @@ public class ConsoleHavkaReporter implements HavkaReporter {
         throw new IllegalArgumentException(String.format("Chat with '%s' chat id is absent", id));
     }
 
-    public void printReport() throws Exception {
+    public String calculateVotes() throws Exception {
         Chat chat = consoleHavkaReporter.getSkypeChat(Constants.HAVATCHATID);
 
-        ChatMessage[] chatMessages = chat.getRecentChatMessages();
+        ChatMessage[] chatMessages = chat.getAllChatMessages();
 
         List<ChatMessage> filteredTodayChatMessagesList = ChatMessageFilter.getMorningMessagesList(ChatMessageFilter.getTodayStartDate(),
                 ChatMessageFilter.getTodayEndDate(),
                 chatMessages);
 
-        List<ChatMessage> negativeVotedMessagesList = new ChatMessageFilter().getVotedMessagesList("yes", filteredTodayChatMessagesList);
-        List<User> negativeUsersList = ChatMessageFilter.getVotedUsersList(negativeVotedMessagesList);
-        UserGroups negativeUserGroups = UserGroups.calculatePeople(negativeUsersList);
+        List<ChatMessage> positiveVotedMessagesList = new ChatMessageFilter().getVotedMessagesList("yes", filteredTodayChatMessagesList);
+        List<ChatMessage> negativeVotedMessagesList = new ChatMessageFilter().getVotedMessagesList("no", filteredTodayChatMessagesList);
 
+        List<User> positiveUsersList = ChatMessageFilter.getVotedUsersListFromChatMessagesList(positiveVotedMessagesList);
+
+        List<User> negativeUsersList = ChatMessageFilter.getVotedUsersListFromChatMessagesList(negativeVotedMessagesList);
+
+        UserGroups positiveUserGroups = UserGroups.calculatePeople(positiveUsersList);
+
+        StringBuilder stringBuilder = new StringBuilder();
         for(int i = 1; i<=4; i++){
-            Printer.printGroup(i, negativeUserGroups.getTodayGroup(i));
+            stringBuilder.append(Printer.printGroup(i, positiveUserGroups.getTodayGroup(i)));
         }
+
+        stringBuilder.append(Printer.printNoVotedUserGroup(negativeUsersList));
+
+        List<User> boycottUsersList = UserGroups.getBoycottUsersList(positiveUsersList, negativeUsersList);
+
+        stringBuilder.append(Printer.printBoycottUserGroup(boycottUsersList));
+        stringBuilder.append("\n");
+
+        return stringBuilder.toString();
+    }
+
+
+    public void printReport() throws SkypeException {
+        Skype.setDaemon(false);
+
+        Skype.addChatMessageListener(new ChatMessageAdapter() {
+            public void chatMessageReceived(ChatMessage received)
+                    throws SkypeException {
+
+                String id = received.getChat().getId();
+                String userMessage ="";
+
+                System.out.println(id);
+                //beseda2 chat
+                if(id.equals("19:a33efa779e3b4eb680af32f1ef1c745a@thread.skype")){
+                    userMessage = received.getContent();
+                    if(userMessage.equalsIgnoreCase(Constants.PRINTREPORT)){
+                        try {
+                            String resultReport = calculateVotes();
+                            System.out.println(resultReport);
+                            Skype.chat(received.getSender().getId()).send(resultReport);
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    else if(userMessage.equalsIgnoreCase(Constants.CLOSEPROGRAM)){
+                        closeProgram();
+                    }
+                    else{
+                        Skype.chat(received.getSender().getId()).send(String.format("'%s' - unsupported command, use 'report' command", userMessage));
+                    }
+                }
+            }
+        });
     }
 
     public void closeProgram() {
