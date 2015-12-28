31ebc0a797ff4ede009581ca3175f32f8eee54ac
gene <qa.in.gene@gmail.com>
2015-12-28 13:27:29 +0200

    UserGroups was updated

--- src/main/java/com/kulikov/havkareporter/UserGroups.java
+++ src/main/java/com/kulikov/havkareporter/UserGroups.java
@@ -4,12 +4,11 @@ package com.kulikov.havkareporter;
  * Created by Gene on 23-12-2015.
  */
 
-import com.skype.SkypeException;
+import com.skype.Skype;
 import com.skype.User;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Enumeration;
 import java.util.List;
 
 public class UserGroups {
@@ -73,5 +72,28 @@ public class UserGroups {
         else {
             throw new IllegalArgumentException(String.format("%s - unsupported group number", groupNumber));
         }
+    }
+
+    public static List<User> getAllUserNamesListFromTemplates(){
+        List<String> allUserNamesList = new ArrayList<String>();
+        allUserNamesList.addAll(Arrays.asList(firstTemplateGroup));
+        allUserNamesList.addAll(Arrays.asList(secondTemplateGroup));
+        allUserNamesList.addAll(Arrays.asList(thirdTemplateGroup));
+        allUserNamesList.addAll(Arrays.asList(fourthTemplateGroup));
+
+        List<User> usersFrom4Groups = new ArrayList<User>();
+        for(String username: allUserNamesList){
+            usersFrom4Groups.add(Skype.getUser(username));
+
+        }
+        return usersFrom4Groups;
+    }
+
+    public static List<User> getBoycottUsersList(List<User> positiveVotedMessagesList, List<User> negativeVotedMessagesList) {
+        List<User> boycottUsersList = new ArrayList<User>();
+        boycottUsersList = UserGroups.getAllUserNamesListFromTemplates();
+        boycottUsersList.removeAll(positiveVotedMessagesList);
+        boycottUsersList.removeAll(negativeVotedMessagesList);
+        return boycottUsersList;
     }
 }
